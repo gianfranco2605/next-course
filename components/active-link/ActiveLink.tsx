4bb6ec89396cdb5d1cbdb9dc4439052234f62f09
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-12-30 13:07:02 +0100

    introduction

---
+++ components/active-link/ActiveLink.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import style from "./ActiveLink.module.css";
+
+// If i put a ? after the key means would be optional prop if not is mandatory(typescrit rule)
+interface Props {
+  path: string;
+  text: string;
+}
+
+export const ActiveLink = ({ path, text }): Props => {
+  const pathName = usePathname();
+
+  return (
+    <Link
+      href={path}
+      className={`${style.link}  ${pathName === path && style["active-link"]}`}
+    >
+      {text}
+    </Link>
+  );
+};
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,21 +1,27 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
 import React from "react";
+import { ActiveLink } from "../active-link/ActiveLink";
+
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/contact", text: "Contact" },
+  { path: "/pricing", text: "Pricing" },
+];
 
 export const Navbar = () => {
   console.log("hello");
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={"/"} className="flex gap-2 items-center">
+        <HomeIcon />
+        <span>Home</span>
+      </Link>
       <div className="flex flex-1"></div>
-      <a className="mr-2" href="/about">
-        About
-      </a>
-      <a className="mr-2" href="/contact">
-        Contact
-      </a>
-      <a className="mr-2" href="/pricing">
-        pricing
-      </a>
+      {navItems.map((navItem) => (
+        <ActiveLink key={navItem.path} {...navItem} />
+      ))}
     </nav>
   );
 };
